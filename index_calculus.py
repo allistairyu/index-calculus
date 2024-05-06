46bca41225ea52c8a9f5cad73c536a2f899099d4
allistairyu <allistair_yu@brown.edu>
2024-05-05 21:44:41 -0400

    quick fix for now: repeat until combined_solution valid

--- index_calculus.py
+++ index_calculus.py
@@ -3,14 +3,14 @@ from random import randint
 from sage.all import *
 from collections import defaultdict 
 
-# PARAM_P = 18443
-# B = 5
-# g = 37
-# h = 211
-PARAM_P = 941
+PARAM_P = 18443
 B = 5
-g = 627
-h = 18
+g = 37
+h = 211
+# PARAM_P = 2003
+# B = 13
+# g = 5
+# h = 543
 
 # get factor base
 # https://stackoverflow.com/a/3035188
@@ -40,7 +40,6 @@ def factor_list_to_dict(factor_list):
 
 def index_calculus(g, h, p, B, factor_base):
     # Step 1: Find a prime factor base B
-    # print(factor_base)
     relations = []
     bases = set()
     while len(relations) <= len(factor_base):
@@ -52,9 +51,9 @@ def index_calculus(g, h, p, B, factor_base):
             relations.append((factors_dict, e))
             for f in factors_dict.keys():
                 bases.add(f)
-    print(relations)
-    print(bases)
-    return bases, relations
+    print('relations = ' + str([(dict(r[0]), r[1]) for r in relations]))
+    print('bases = ' + str(bases))
+    return list(bases), relations
 
 def to_matrices(bases, congruences):
     M = [[c[0][base] if base in c[0] else 0 \
@@ -69,34 +68,53 @@ def solve_matrix(M, b, p):
     b = vector(R, b)
     return M.solve_right(b)
 
-def main():
+def check_solution(g, factors, solution):
+    for i in range(len(factors)):
+        f = factors[i]
+        s = solution[i]
+        print(f'Checking factor g^{s} == {f} (mod {PARAM_P})')
+        # print(pow(g, solution[i], PARAM_P) == f)
+        if pow(g, solution[i], PARAM_P) != f:
+            return False
+    return True
+
+def check_dlog(g, h, x):
+    return pow(g, x, PARAM_P) == h
+
+def main_func():
+    print(f'PARAM_P: {PARAM_P}, B: {B}, g: {g}, h: {h}')
     factor_base = primes1(B + 1)
-    bases, relations = index_calculus(g, h, PARAM_P, B, factor_base)
-    # bases = {2,3,5}
-    # relations = [({2: 3, 3: 4, 5: 1}, 12708), ({2: 3, 5: 2}, 11311), ({2: 3, 3: 3, 5: 1}, 15400), ({2: 3, 3: 1, 5: 4}, 2731)]
-    M, b = to_matrices(bases, relations)
+    while True:
+        bases, relations = index_calculus(g, h, PARAM_P, B, factor_base)
+        # relations = [({3: 3, 5: 2}, 4478), ({2: 2, 3: 4, 5: 2}, 13252), ({2: 12, 3: 1}, 10778), ({2: 1, 5: 1}, 12010)]
+        # bases = [2, 3, 5]
+        M, b = to_matrices(bases, relations)
 
-    solutions = []
-    factors = list(set(primefac(PARAM_P - 1))) # TODO: generalize
-    for f in factors:
-        solutions.append([int(i) for i in solve_matrix(M, b, f)])
+        solutions = []
+        factors = list(set(primefac(PARAM_P - 1))) # TODO: generalize
+        for f in factors:
+            s = [int(i) for i in solve_matrix(M, b, f)]
+            print(s)
+            solutions.append(s)
 
-    combined_solution = []
-    residues = list(zip(*solutions)) # transpose
+        combined_solution = []
+        residues = list(zip(*solutions)) # transpose
 
-    for i in range(len(residues)):
-        combined_solution.append(CRT(list(residues[i]), factors))
+        for i in range(len(residues)):
+            combined_solution.append(CRT(list(residues[i]), factors))
 
-    print(combined_solution)
-    # combined_solution = [5733,15750,6277]
+        if check_solution(g, bases, combined_solution):
+            print('combined solution: ' + str(combined_solution))
+            break
+
+    # print(check_solution(g, bases, combined_solution))
     
     # TODO: check solution
     k = 0
     factors = []
     while True:
         k = randint(1, PARAM_P - 1)
-        # k = 9549
-        # k = 6004
+        # k = 2223
         hgk = (h * pow(g, -k, PARAM_P)) % PARAM_P
         B_smooth, factors = check_B_smooth(hgk, B)
         if B_smooth:
@@ -111,7 +129,8 @@ def main():
         result = (result + factors_dict[factor_base[i]] * combined_solution[i]) % (PARAM_P - 1)
 
     print(result)
+    # print(check_dlog(g, h, result))
 
 
 if __name__ == '__main__':
-    main()
+    main_func()
