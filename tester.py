fbf9de16852ac2e37574a009868833c6ce6b101d
allistairyu <allistair_yu@brown.edu>
2024-05-05 21:45:24 -0400

    run against bsgs

--- tester.py
+++ tester.py
@@ -1,5 +1,18 @@
 from bsgs import bsgs
-from index_calculus import index_calculus
+from index_calculus import main_func
+import time
+
+PARAM_P = 18443
+B = 5
+g = 37
+h = 211
 
 if __name__ == '__main__':
-	print('hello world')
+	start_time = time.time()
+	main_func()
+	print("index calc: --- %s seconds ---" % (time.time() - start_time))
+
+	start_time = time.time()
+	bsgs(g, h, PARAM_P)
+	print("bsgs      : --- %s seconds ---" % (time.time() - start_time))
+
