ed23364277577f3c928cfadcb2cd5a8846624538
allistairyu <allistair_yu@brown.edu>
2024-05-10 09:53:38 -0400

    fixed

--- index_calculus.py
+++ index_calculus.py
@@ -3,20 +3,15 @@ from random import randint
 from sage.all import *
 from collections import defaultdict 
 
-PARAM_P = 18443
 B = 5
-g = 37
-h = 211
-# PARAM_P = 2003
-# B = 13
-# g = 5
-# h = 543
+factor_base = [2, 3, 5]
 
-# get factor base
+# get factor base for B
 # https://stackoverflow.com/a/3035188
 # TODO: implement sieve of eratosthenes
 def primes1(n):
-    """ Returns  a list of primes < n """
+    """ Returns  a list of primes <= n """
+    n += 1
     sieve = [True] * (n//2)
     for i in range(3,int(n**0.5)+1,2):
         if sieve[i//2]:
@@ -30,16 +25,37 @@ def check_B_smooth(n, B):
             return False, factors
     return True, factors
 
+def to_matrices(bases, congruences):
+    M = [[c[0][base] if base in c[0] else 0 \
+            for base in bases] for c in congruences]
+    b = [c[1] for c in congruences]
+    return M, b
+
+def solve_matrix(M, b, p):
+    R = IntegerModRing(p)
+    M = matrix(R, M)
+    b = vector(R, b)
+    return M.solve_right(b)
+
 def factor_list_to_dict(factor_list):
     factor_dict = defaultdict(lambda: 0)
     for factor in factor_list:
-        # if factor not in factor_dict:
-        #     factor_dict[factor] = 0
         factor_dict[factor] += 1
     return factor_dict
 
-def index_calculus(g, h, p, B, factor_base):
-    # Step 1: Find a prime factor base B
+def check_solution(g, factors, solution, p):
+    for i in range(len(factors)):
+        f = factors[i]
+        s = solution[i]
+        # print(f'Checking {g}^{s} = {pow(g, s, p)} = {f}')
+        if pow(g, s, p) != f:
+            return False
+    return True
+
+def check_dlog(g, h, x, p):
+    return pow(g, x, p) == h
+
+def compute_relations(g, p):
     relations = []
     bases = set()
     while len(relations) <= len(factor_base):
@@ -51,86 +67,46 @@ def index_calculus(g, h, p, B, factor_base):
             relations.append((factors_dict, e))
             for f in factors_dict.keys():
                 bases.add(f)
-    print('relations = ' + str([(dict(r[0]), r[1]) for r in relations]))
-    print('bases = ' + str(bases))
     return list(bases), relations
 
-def to_matrices(bases, congruences):
-    M = [[c[0][base] if base in c[0] else 0 \
-            for base in bases] for c in congruences]
-    b = [c[1] for c in congruences]
-    return M, b
-
-def solve_matrix(M, b, p):
-    
-    R = IntegerModRing(p)
-    M = matrix(R, M)
-    b = vector(R, b)
-    return M.solve_right(b)
-
-def check_solution(g, factors, solution):
-    for i in range(len(factors)):
-        f = factors[i]
-        s = solution[i]
-        print(f'Checking factor g^{s} == {f} (mod {PARAM_P})')
-        # print(pow(g, solution[i], PARAM_P) == f)
-        if pow(g, solution[i], PARAM_P) != f:
-            return False
-    return True
-
-def check_dlog(g, h, x):
-    return pow(g, x, PARAM_P) == h
-
-def main_func():
-    print(f'PARAM_P: {PARAM_P}, B: {B}, g: {g}, h: {h}')
-    factor_base = primes1(B + 1)
+def compute_small_dlogs(g, p):
     while True:
-        bases, relations = index_calculus(g, h, PARAM_P, B, factor_base)
-        # relations = [({3: 3, 5: 2}, 4478), ({2: 2, 3: 4, 5: 2}, 13252), ({2: 12, 3: 1}, 10778), ({2: 1, 5: 1}, 12010)]
-        # bases = [2, 3, 5]
+        bases, relations = compute_relations(g, p)
         M, b = to_matrices(bases, relations)
-
+        
         solutions = []
-        factors = list(set(primefac(PARAM_P - 1))) # TODO: generalize
+        factors = list(set(primefac(p - 1)))
         for f in factors:
-            s = [int(i) for i in solve_matrix(M, b, f)]
-            print(s)
-            solutions.append(s)
+            solutions.append([int(i) for i in solve_matrix(M, b, f)])
 
         combined_solution = []
-        residues = list(zip(*solutions)) # transpose
-
+        residues = list(zip(*solutions))
         for i in range(len(residues)):
             combined_solution.append(CRT(list(residues[i]), factors))
-
-        if check_solution(g, bases, combined_solution):
-            print('combined solution: ' + str(combined_solution))
+        
+        if check_solution(g, bases, combined_solution, p):
             break
+    return combined_solution
 
-    # print(check_solution(g, bases, combined_solution))
-    
-    # TODO: check solution
-    k = 0
+def solve(g, h, p):
+    print(f'Solving {g}^x = {h} mod {p}...')
+    small_dlogs = compute_small_dlogs(g, p)
+    k = 2
     factors = []
     while True:
-        k = randint(1, PARAM_P - 1)
-        # k = 2223
-        hgk = (h * pow(g, -k, PARAM_P)) % PARAM_P
+        hgk = (h * pow(g, -k, p)) % p
         B_smooth, factors = check_B_smooth(hgk, B)
         if B_smooth:
-            print('k: ' + str(k))
-            print('factors: ' + str(factors))
             break
+        k += 1
     
     factors_dict = factor_list_to_dict(factors)
-    
-    result = k
-    for i in range(len(combined_solution)):
-        result = (result + factors_dict[factor_base[i]] * combined_solution[i]) % (PARAM_P - 1)
-
-    print(result)
-    # print(check_dlog(g, h, result))
-
+    x = k
+    for i in range(len(small_dlogs)):
+        x = (x + factors_dict[factor_base[i]] * small_dlogs[i]) % (p - 1)
+    if check_dlog(g, h, x, p):
+        print(f'Solution found: x = {x}')
+    return x
 
 if __name__ == '__main__':
-    main_func()
+    solve(37, 211, 18443)
