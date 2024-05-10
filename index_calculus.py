53eeaba88f7ae71049a95f4dcc2261ea93e92b29
allistairyu <allistair_yu@brown.edu>
2024-05-10 10:03:38 -0400

    final

--- index_calculus.py
+++ index_calculus.py
@@ -46,7 +46,6 @@ def check_solution(g, factors, solution, p):
     for i in range(len(factors)):
         f = factors[i]
         s = solution[i]
-        # print(f'Checking {g}^{s} = {pow(g, s, p)} = {f}')
         if pow(g, s, p) != f:
             return False
     return True
@@ -89,23 +88,24 @@ def compute_small_dlogs(g, p):
 
 def solve(g, h, p):
     print(f'Solving {g}^x = {h} mod {p}...')
-    small_dlogs = compute_small_dlogs(g, p)
-    k = 1
-    factors = []
     while True:
-        hgk = (h * pow(g, -k, p)) % p
-        B_smooth, factors = check_B_smooth(hgk, B)
-        if B_smooth:
-            break
-        k += 1
-    
-    factors_dict = factor_list_to_dict(factors)
-    x = k
-    for i in range(len(small_dlogs)):
-        x = (x + factors_dict[factor_base[i]] * small_dlogs[i]) % (p - 1)
-    if check_dlog(g, h, x, p):
-        print(f'Solution found: x = {x}')
-    return x
+        small_dlogs = compute_small_dlogs(g, p)
+        k = randint(1, p-1)
+        factors = []
+        while True:
+            hgk = (h * pow(g, -k, p)) % p
+            B_smooth, factors = check_B_smooth(hgk, B)
+            if B_smooth:
+                break
+            k = randint(1, p-1)
+        
+        factors_dict = factor_list_to_dict(factors)
+        x = k
+        for i in range(len(small_dlogs)):
+            x = (x + factors_dict[factor_base[i]] * small_dlogs[i]) % (p - 1)
+        if check_dlog(g, h, x, p):
+            print(f'Solution: x = {x}')
+            return x
 
 if __name__ == '__main__':
     solve(37, 211, 18443)
